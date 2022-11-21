02dc5f3ffe59ccd8deecea907532f80d1ef9a5ab
Tenable.cs <admin@accurics.com>
2022-11-21 17:55:27 +0900

    Auto Remediation By Tenable.cs

--- AZ_LoadBalancer_Basic_VM/compute.tf
+++ AZ_LoadBalancer_Basic_VM/compute.tf
@@ -1,7 +1,7 @@
 resource "azurerm_availability_set" "contoso_availability_set" {
-    name = "contoso_availability_set"
-    location = azurerm_resource_group.eastcoast-rg.location
-    resource_group_name = azurerm_resource_group.eastcoast-rg.name
+  name                = "contoso_availability_set"
+  location            = azurerm_resource_group.eastcoast-rg.location
+  resource_group_name = azurerm_resource_group.eastcoast-rg.name
 }
 
 
@@ -9,11 +9,11 @@ resource "azurerm_windows_virtual_machine" "boston-vm" {
   name                  = "boston-vm"
   location              = azurerm_resource_group.eastcoast-rg.location
   resource_group_name   = azurerm_resource_group.eastcoast-rg.name
-  admin_username = var.username
-  admin_password = var.vmPasswrd
+  admin_username        = var.username
+  admin_password        = var.vmPasswrd
   network_interface_ids = [azurerm_network_interface.boston-nic.id]
   size                  = "Standard_B2ms"
-  availability_set_id = azurerm_availability_set.contoso_availability_set.id
+  availability_set_id   = azurerm_availability_set.contoso_availability_set.id
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
@@ -23,8 +23,8 @@ resource "azurerm_windows_virtual_machine" "boston-vm" {
   }
 
   os_disk {
-    name              = "osdisk1"
-    caching           = "ReadWrite"
+    name                 = "osdisk1"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 }
@@ -34,12 +34,12 @@ resource "azurerm_windows_virtual_machine" "atlanta-vm" {
   name                  = "atlanta-vm"
   location              = azurerm_resource_group.eastcoast-rg.location
   resource_group_name   = azurerm_resource_group.eastcoast-rg.name
-  admin_username = var.username
-  admin_password = var.vmPasswrd
+  admin_username        = var.username
+  admin_password        = var.vmPasswrd
   network_interface_ids = [azurerm_network_interface.atlanta-nic.id]
   size                  = "Standard_B2ms"
-  availability_set_id = azurerm_availability_set.contoso_availability_set.id
- 
+  availability_set_id   = azurerm_availability_set.contoso_availability_set.id
+
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
@@ -49,27 +49,32 @@ resource "azurerm_windows_virtual_machine" "atlanta-vm" {
   }
 
   os_disk {
-    name              = "osdisk2"
-    caching           = "ReadWrite"
+    name                 = "osdisk2"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+  }
+
+  boot_diagnostics {
+    enabled     = true
+    storage_uri = "<storage_uri>"
   }
 }
 
 
 resource "azurerm_virtual_machine_extension" "boston_vm_ext_install_iis" {
   name                       = "boston_vm_extension_install_iis"
-  virtual_machine_id         =  azurerm_windows_virtual_machine.boston-vm.id
+  virtual_machine_id         = azurerm_windows_virtual_machine.boston-vm.id
   publisher                  = "Microsoft.Compute"
   type                       = "CustomScriptExtension"
   type_handler_version       = "1.8"
   auto_upgrade_minor_version = true
 
-  settings = <<SETTINGS
+  settings   = <<SETTINGS
     {
         "commandToExecute":"powershell -ExecutionPolicy Unrestricted New-NetFirewallRule -Display allowHTTP -Direction Inbound -LocalPort 80 -Protocol TCP -Action All; powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
     }
 SETTINGS
-depends_on = [azurerm_windows_virtual_machine.atlanta-vm]
+  depends_on = [azurerm_windows_virtual_machine.atlanta-vm]
 }
 
 
@@ -81,10 +86,10 @@ resource "azurerm_virtual_machine_extension" "atlanta_vm_ext_install_iis" {
   type_handler_version       = "1.8"
   auto_upgrade_minor_version = true
 
-  settings = <<SETTINGS
+  settings   = <<SETTINGS
     {
         "commandToExecute": "powershell -ExecutionPolicy Unrestricted New-NetFirewallRule -Display allowHTTP -Direction Inbound -LocalPort 80 -Protocol TCP -Action All; powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
     }
 SETTINGS
-depends_on = [azurerm_windows_virtual_machine.boston-vm]
+  depends_on = [azurerm_windows_virtual_machine.boston-vm]
 }
